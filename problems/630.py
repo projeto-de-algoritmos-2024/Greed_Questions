96641c485fc2b040ed7e01f9ec012640ac85ad1c
martinsglucas <martin5.luca5.univ@gmail.com>
2025-01-18 23:51:24 -0300

    controla inicio e fim de cada curso

--- problems/630.py
+++ problems/630.py
@@ -2,13 +2,13 @@ from typing import List
 
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
-        courses.sort(key=lambda x: x[1])
+        courses.sort(key=lambda x: (x[1], x[0]))
         time = 0
-        result = 0
+        start_and_finish = []
         for duration, deadline in courses:
             time += duration
-            result += 1
-            if deadline < time:
-                result -= 1
-                return result
-        return result
+            start_and_finish.append([time, time+duration])
+            if time > deadline:
+                time -= duration
+                start_and_finish.pop()
+        return len(start_and_finish)
