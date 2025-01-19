919b6c758624dfe1568f59bc2bc21a670f9c7a71
martinsglucas <martin5.luca5.univ@gmail.com>
2025-01-19 00:10:14 -0300

    corrige incremento do dia

--- problems/630.py
+++ problems/630.py
@@ -3,12 +3,13 @@ from typing import List
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
         courses.sort(key=lambda x: (x[1], x[0]))
-        time = 0
+        day = 0
         start_and_finish = []
         for duration, deadline in courses:
-            time += duration
-            start_and_finish.append([time, time+duration])
-            if time > deadline:
-                time -= duration
+            start_and_finish.append([day, day+duration])
+            day += duration
+            if day > deadline:
+                day -= duration
                 start_and_finish.pop()
+                break
         return len(start_and_finish)
