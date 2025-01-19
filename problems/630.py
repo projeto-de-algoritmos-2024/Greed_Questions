3210f5dc4d4802cade6cb667b1cb55a8ddb57747
martinsglucas <martin5.luca5.univ@gmail.com>
2025-01-19 00:14:37 -0300

    troca vetor por heap (accepted)

--- problems/630.py
+++ problems/630.py
@@ -1,15 +1,14 @@
 from typing import List
+import heapq
 
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
-        courses.sort(key=lambda x: (x[1], x[0]))
+        courses.sort(key=lambda x: x[1])
+        heap = []
         day = 0
-        start_and_finish = []
         for duration, deadline in courses:
-            start_and_finish.append([day, day+duration])
             day += duration
+            heapq.heappush(heap, -duration)
             if day > deadline:
-                day -= duration
-                start_and_finish.pop()
-                break
-        return len(start_and_finish)
+                day += heapq.heappop(heap)
+        return len(heap)
